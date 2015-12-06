bbca4db8a1c7bcf74c11601bd9475eda09a08b47
guuhworship <g.92oliveira@gmail.com>
2015-12-06 03:10:50 -0200

    Site pessoal

---
+++ _scripts/funcoes.js
@@ -0,0 +1,13 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function mudafoto(foto){
+    document.getElementById("icone").src = foto;
+}
+
+function changeStyleSheet(sheet){
+    document.getElementById("pageStyle").href = sheet;
+}
